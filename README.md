f076872e6f3328ffa3fcf943219cd3e1dc720af6
dwimnofan <dwi.nofan@students.paramadina.ac.id>
2021-11-19 23:56:34 +0700

    fix path routing

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# express-app
+# express-app - Tugas Pertemuan 10 AP3 Node Js
+
+## Dwi Muhamad Nofan
+
+Jika ada _Error: ENOENT: no such file or directory, stat '/path file html'_ ketika akses URL setelah running server maka hapus "/express-app" di controllers/pages.js
--- controllers/pages.js
+++ controllers/pages.js
@@ -3,13 +3,13 @@ import path  from 'path';
 const __dirname = path.resolve();
 
 export const Home = (req, res) => {
-    res.sendFile('./Pertemuan 10/views/index.html', { root: __dirname });
+    res.sendFile('./express-app/views/index.html', { root: __dirname });
 }
 
 export const About = (req, res) => {
-    res.sendFile('./Pertemuan 10/views/about.html', { root: __dirname });
+    res.sendFile('./express-app/views/about.html', { root: __dirname });
 }
 
 export const Contact = (req, res) => {
-    res.sendFile('./Pertemuan 10/views/contact.html', { root: __dirname });
+    res.sendFile('./express-app/views/contact.html', { root: __dirname });
 }
